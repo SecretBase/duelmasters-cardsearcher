2115aa0cdd072cc93225295b7a7f7222965d9441
SecondHong <whpigpig@gmail.com>
2015-01-17 00:44:48 +0800

    Update a bit of CardModel

--- application/models/cardmodel.php
+++ application/models/cardmodel.php
@@ -2,12 +2,32 @@
 
 class CardModel extends CI_Model {
 
-	public function add_log()
+	private $table = 'card';
+
+	function __construct()
 	{
-		
+		parent::__construct();
+
+		$this->load->model('RaceModel');
 	}
 
-	public function get_log()
+	public function get_cards($criteria = array())
+	{
+		if ( ! empty($criteria['race_id']))
+			$this->db->where('race_id', $criteria['race_id']);
+	}
+
+	public function create_card()
+	{
+
+	}
+
+	public function delete_card()
+	{
+
+	}
+
+	public function update_card()
 	{
 
 	}
